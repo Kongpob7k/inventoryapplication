654373ad1d56054447861dee76862644ea1f5475
Kongpob7k <kongpopteen@gmail.com>
2025-07-29 21:49:53 +0700

    Second Commit

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    database:"inventory",
-    password:process.env.DB_PASSWORD,
-    port:5432
+    connectionString : process.env.DATABASE_URL
 })
